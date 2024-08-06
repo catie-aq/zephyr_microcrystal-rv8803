7e5d9b1becaa21706c0b83e954584b82af11a20b
l_saintgermain_catie <l.saintgermain@catie.fr>
2024-08-06 14:13:20 +0200

    Added basic RTC API

--- drivers/rtc/rv_8803_c7/rv_8803_c7.h
+++ drivers/rtc/rv_8803_c7/rv_8803_c7.h
@@ -3,7 +3,42 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef ZEPHYR_DRIVERS_SENSOR_RV_8803_C7_RV_8803_C7_H_
-#define ZEPHYR_DRIVERS_SENSOR_RV_8803_C7_RV_8803_C7_H_
+#ifndef ZEPHYR_DRIVERS_RTC_MICROCRYSTAL_RV_8803_C7_H_
+#define ZEPHYR_DRIVERS_RTC_MICROCRYSTAL_RV_8803_C7_H_
 
-#endif /* ZEPHYR_DRIVERS_SENSOR_RV_8803_C7_RV_8803_C7_H_ */
+#include <zephyr/drivers/rtc.h>
+#include <zephyr/drivers/i2c.h>
+
+/* Registers */
+#define RV8803C7_REGISTER_SECONDS   0x00
+#define RV8803C7_REGISTER_MINUTES   0x01
+#define RV8803C7_REGISTER_HOURS     0x02
+#define RV8803C7_REGISTER_WEEKDAY   0x03
+#define RV8803C7_REGISTER_DATE      0x04
+#define RV8803C7_REGISTER_MONTH     0x05
+#define RV8803C7_REGISTER_YEAR      0x06
+
+/* Data masks */
+#define RV8803C7_SECONDS_BITS   GENMASK(6, 0)
+#define RV8803C7_MINUTES_BITS   GENMASK(6, 0)
+#define RV8803C7_HOURS_BITS     GENMASK(5, 0)
+#define RV8803C7_WEEKDAY_BITS   GENMASK(6, 0)
+#define RV8803C7_DATE_BITS      GENMASK(5, 0)
+#define RV8803C7_MONTH_BITS     GENMASK(4, 0)
+#define RV8803C7_YEAR_BITS      GENMASK(7, 0)
+
+/* Control MACRO */
+#define RV8803C7_PARTIAL_SECONDS_INCR 59 // Check for partial incrementation when reads get 59 seconds
+#define RV8803C7_CORRECT_YEAR_LEAP_MIN (2000 - 1900) // Diff between 2000 and tm base year 1900
+#define RV8803C7_CORRECT_YEAR_LEAP_MAX (2099 - 1900) // Diff between 2099 and tm base year 1900
+
+/* Structs */
+struct rv_8803_c7_config {
+	struct i2c_dt_spec i2c_bus;
+};
+
+/* API */
+static int rv_8803_c7_set_time(const struct device *dev, const struct rtc_time *timeptr);
+static int rv_8803_c7_get_time(const struct device *dev, struct rtc_time *timeptr);
+
+#endif /* ZEPHYR_DRIVERS_RTC_MICROCRYSTAL_RV_8803_C7_H_ */
--- samples/src/main.c
+++ samples/src/main.c
@@ -5,23 +5,23 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/rtc.h>
 
 int main(void)
 {
-	const struct device *const dev = DEVICE_DT_GET_ANY(micro crystal_rv_8803_c7);
-	struct sensor_value value;
+	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(rv_8803_c70));
+	//struct rtc_time *sample_time;
 
 	if (!device_is_ready(dev)) {
-		printk("Device %s is not ready\n", dev->name);
+		printk("Device is not ready\r\n");
 		return 1;
 	}
 
 	while (1) {
-		sensor_sample_fetch(dev);
+		//sensor_sample_fetch(dev);
         // Example for ambiant temperature sensor
-		sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &value);
-		printk("Value: %d.%06d\n", value.val1, value.val2);
+		//sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &value);
+		printk("Test\r\n");
 		k_sleep(K_MSEC(1000));
 	}
 }
