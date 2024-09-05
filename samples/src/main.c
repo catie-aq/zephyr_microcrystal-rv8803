822eb0c2187de002aa437e0c6749b8097d5addf2
l_saintgermain_catie <l.saintgermain@catie.fr>
2024-09-05 10:29:36 +0200

    Removed '\r'

--- samples/src/main.c
+++ samples/src/main.c
@@ -17,7 +17,7 @@ static const struct device *rtc = DEVICE_DT_GET(DT_NODELABEL(rv88030));
 
 void alarm_callback(const struct device *dev, uint16_t id, void *user_data)
 {
-	printk("RTC Alarm detected!!\r\n");
+	printk("RTC Alarm detected!!\n");
 }
 
 int main(void)
@@ -26,7 +26,7 @@ int main(void)
 	char str[TIME_SIZE];
 
 	if (!device_is_ready(rtc)) {
-		printk("Device is not ready\r\n");
+		printk("Device is not ready\n");
 		return 1;
 	}
 
@@ -34,10 +34,10 @@ int main(void)
 	gmtime_r(&timer_set, (struct tm *)(&datetime_set));
 	memset(&datetime_get, 0xFF, sizeof(datetime_get));
 	if (rtc_set_time(rtc, &datetime_set) != 0) {
-		printk("Failed to set time\r\n");
+		printk("Failed to set time\n");
 	}
 	if (rtc_get_time(rtc, &datetime_get) != 0) {
-		printk("Failed to get time using rtc_time_get()\r\n");
+		printk("Failed to get time using rtc_time_get()\n");
 	}
 
 	datetime_alarm.tm_min = 1;
@@ -45,33 +45,33 @@ int main(void)
 	datetime_alarm.tm_mday = 0;
 	datetime_alarm.tm_wday = 0;
 	if (rtc_alarm_set_time(rtc, 0, RTC_ALARM_TIME_MASK_MINUTE, &datetime_alarm)) {
-		printk("Failed to set alarm time using rtc_alarm_set_time()\r\n");
+		printk("Failed to set alarm time using rtc_alarm_set_time()\n");
 	}
-	printk("Setter[%ld] datetime [%d|%d %d:%d]\r\n", RTC_ALARM_TIME_MASK_MINUTE,
+	printk("Setter[%ld] datetime [%d|%d %d:%d]\n", RTC_ALARM_TIME_MASK_MINUTE,
 	       datetime_alarm.tm_wday, datetime_alarm.tm_mday, datetime_alarm.tm_hour,
 	       datetime_alarm.tm_min);
 
 	uint16_t value = 0;
 	if (rtc_alarm_get_time(rtc, 0, &value, &datetime_alarm)) {
-		printk("Failed to get alarm time using rtc_alarm_get_time()\r\n");
+		printk("Failed to get alarm time using rtc_alarm_get_time()\n");
 	}
-	printk("Getter[%d] datetime [%d|%d %d:%d]\r\n", value, datetime_alarm.tm_wday,
+	printk("Getter[%d] datetime [%d|%d %d:%d]\n", value, datetime_alarm.tm_wday,
 	       datetime_alarm.tm_mday, datetime_alarm.tm_hour, datetime_alarm.tm_min);
 
 	if (rtc_alarm_set_callback(rtc, 0, alarm_callback, NULL)) {
-		printk("Failed to set alarm callback using rtc_alarm_set_callback()\r\n");
+		printk("Failed to set alarm callback using rtc_alarm_set_callback()\n");
 	}
 
 	while (1) {
 		// Example for Real-Time Controller
 		if (rtc_get_time(rtc, &datetime_get) != 0) {
-			printk("Failed to get time using rtc_time_get()\r\n");
+			printk("Failed to get time using rtc_time_get()\n");
 		}
 
 		int value = rtc_alarm_is_pending(rtc, 0);
 
 		strftime(str, TIME_SIZE, "%c", rtc_time_to_tm(&datetime_get));
-		printf("RTC_TIME[%d] [%s]\r\n", value, str);
+		printf("RTC_TIME[%d] [%s]\n", value, str);
 
 		k_sleep(K_MSEC(1000));
 	}
