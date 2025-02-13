d9cd3f74f7d71c1cafec2bc3fb0a456bd62cef5d
l_saintgermain_catie <l.saintgermain@catie.fr>
2025-02-13 10:21:21 +0100

    Refactor RTC's devicetree related macros

--- drivers/rtc/microcrystal/rv8803/rv8803_rtc.h
+++ drivers/rtc/microcrystal/rv8803/rv8803_rtc.h
@@ -53,30 +53,70 @@
 #define RV8803_DISABLE_UPDATE       (0x00 << 5)
 
 /* Structs */
+// clang-format off
+#if defined(CONFIG_RTC_ALARM)
+#if RV8803_HAS_IRQ
+#define RV8803_RTC_IRQ_ALARM_AVAILABLE 1
+#define RV8803_RTC_IRQ_ALARM(inst) DT_INST_ENUM_IDX(inst, alarm)
+#define RV8803_RTC_IRQ_ALARM_HAS_PROP(inst) DT_INST_PROP(inst, use_irq)
+#else
+#define RV8803_RTC_IRQ_ALARM_AVAILABLE 0
+#define RV8803_RTC_IRQ_ALARM(inst) 0
+#define RV8803_RTC_IRQ_ALARM_HAS_PROP(inst) 0
+#endif /* RV8803_HAS_IRQ */
+#endif /* CONFIG_RTC_ALARM */
+
+#if defined(CONFIG_RTC_UPDATE)
+#if RV8803_HAS_IRQ
+#define RV8803_RTC_IRQ_UPDATE_AVAILABLE 1
+#define RV8803_RTC_IRQ_UPDATE(inst) DT_INST_ENUM_IDX(inst, update)
+#define RV8803_RTC_IRQ_UPDATE_HAS_PROP(inst) DT_INST_PROP(inst, use_irq)
+#else
+#define RV8803_RTC_IRQ_UPDATE_AVAILABLE 0
+#define RV8803_RTC_IRQ_UPDATE(inst) 0
+#define RV8803_RTC_IRQ_UPDATE_HAS_PROP(inst) 0
+#endif /* RV8803_HAS_IRQ */
+#endif /* CONFIG_RTC_UPDATE */
+
+#if RV8803_RTC_IRQ_ALARM_AVAILABLE || RV8803_RTC_IRQ_UPDATE_AVAILABLE
+#define RV8803_RTC_IRQ_IN_USE 1
+#define RV8803_RTC_IRQ_HAS_PROP(inst) DT_INST_PROP(inst, use_irq)
+#else
+#define RV8803_RTC_IRQ_IN_USE 0
+#define RV8803_RTC_IRQ_HAS_PROP(inst) 0
+#endif
+// clang-format on
+
 /* RV8803 RTC config */
 struct rv8803_rtc_config {
-	const struct device *base_dev; /* Parent device reference */
+	const struct device *mfd_dev;
+	const bool irq_alarm;
+	const bool irq_update;
 };
 
+#if RV8803_RTC_IRQ_IN_USE
+// clang-format off
 struct rv8803_rtc_irq {
-#if RV8803_IRQ_GPIO_IN_USE
 	const struct device *dev;
-#endif /* RV8803_IRQ_GPIO_IN_USE */
+	struct k_work work;
+	int (*append_listener_fn)(const struct device *dev, struct k_work *worker);
 };
+// clang-format on
+#endif /* RV8803_RTC_IRQ_IN_USE */
 
+#if RV8803_RTC_IRQ_ALARM_AVAILABLE
 struct rv8803_rtc_alarm {
-#if RV8803_IRQ_GPIO_USE_ALARM
 	rtc_alarm_callback alarm_cb;
 	void *alarm_cb_data;
-#endif /* RV8803_IRQ_GPIO_USE_ALARM */
 };
+#endif /* RV8803_RTC_IRQ_ALARM_AVAILABLE */
 
+#if RV8803_RTC_IRQ_UPDATE_AVAILABLE
 struct rv8803_rtc_update {
-#if RV8803_IRQ_GPIO_USE_UPDATE
 	rtc_update_callback update_cb;
 	void *update_cb_data;
-#endif /* RV8803_IRQ_GPIO_USE_UPDATE */
 };
+#endif /* RV8803_RTC_IRQ_UPDATE_AVAILABLE */
 
 /* RV8803 RTC data */
 struct rv8803_rtc_data {
